6c3730d1ac48c33a0619ab41376e6ea45cd0bd8f
Tushar Jhod <115688635+kaiser4551@users.noreply.github.com>
2024-05-29 11:01:13 +0530

    Time: 40 ms (87.71%) | Memory: 17.1 MB (76.25%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,27 +1,34 @@
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         ROWS, COLS = len(matrix), len(matrix[0])
+
+#         top, bot = 0, ROWS - 1
+#         while top <= bot:
+#             row = (bot-top) // 2+top
+#             if target > matrix[row][-1]:
+#                 top = row + 1
+#             elif target < matrix[row][0]:
+#                 bot = row - 1
+#             else:
+#                 break
+
+#         if not (top <= bot):
+#             return False
+#         row = (bot-top) // 2+top
+#         l, r = 0, COLS - 1
+#         while l <= r:
+#             m = (r - l) // 2 + l
+#             if target > matrix[row][m]:
+#                 l = m + 1
+#             elif target < matrix[row][m]:
+#                 r = m - 1
+#             else:
+#                 return True
+#         return False
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROWS, COLS = len(matrix), len(matrix[0])
-
-        top, bot = 0, ROWS - 1
-        while top <= bot:
-            row = (bot-top) // 2+top
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
-            else:
-                break
-
-        if not (top <= bot):
-            return False
-        row = (bot-top) // 2+top
-        l, r = 0, COLS - 1
-        while l <= r:
-            m = (r - l) // 2 + l
-            if target > matrix[row][m]:
-                l = m + 1
-            elif target < matrix[row][m]:
-                r = m - 1
-            else:
-                return True
+        for item in matrix:
+            for item2 in item:
+                if item2 == target:
+                    return True
         return False
