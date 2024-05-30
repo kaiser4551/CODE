ca0584149a88a52f15fbffc2c5c130efb57e66e7
Tushar Jhod <115688635+kaiser4551@users.noreply.github.com>
2024-05-30 11:42:25 +0530

    Time: 54 ms (84.90%) | Memory: 17.6 MB (60.29%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -6,15 +6,31 @@
 #                 return [required[target - nums[i]],i]
 #             else:
 #                 required[nums[i]]=i
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         numMap = {}
+#         n = len(nums)
+
+#         for i in range(n):
+#             complement = target - nums[i]
+#             if complement in numMap:
+#                 return [numMap[complement], i]
+#             numMap[nums[i]] = i
+
+#         return []  # No solution found
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numMap = {}
         n = len(nums)
 
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
         for i in range(n):
             complement = target - nums[i]
-            if complement in numMap:
-                return [numMap[complement], i]
-            numMap[nums[i]] = i
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
 
         return []  # No solution found
