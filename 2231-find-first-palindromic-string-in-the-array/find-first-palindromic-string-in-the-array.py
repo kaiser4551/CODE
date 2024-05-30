5bd0bcf7a2ba82f820f9599a237c89dbac6dbb8a
Tushar Jhod <115688635+kaiser4551@users.noreply.github.com>
2024-05-30 16:19:46 +0530

    Time: 69 ms (87.05%) | Memory: 16.6 MB (68.94%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -1,24 +1,30 @@
-class Solution:
+# class Solution:
     
-    def isPalindrome(self, s: str) -> bool:
-        s = s.lower()
+#     def isPalindrome(self, s: str) -> bool:
+#         s = s.lower()
 
-        left, right = 0, len(s)-1
-        while left < right:
-            if not s[left].isalnum():
-                left += 1            
-            elif not s[right].isalnum():
-                right -= 1            
-            else:
-                if s[left] != s[right]:
-                    return False
-                else:
-                    left += 1
-                    right -= 1
-        return True 
+#         left, right = 0, len(s)-1
+#         while left < right:
+#             if not s[left].isalnum():
+#                 left += 1            
+#             elif not s[right].isalnum():
+#                 right -= 1            
+#             else:
+#                 if s[left] != s[right]:
+#                     return False
+#                 else:
+#                     left += 1
+#                     right -= 1
+#         return True 
 
+#     def firstPalindrome(self, words: List[str]) -> str:
+#         for i in range(len(words)):
+#             if self.isPalindrome(words[i]) is True:
+#                 return words[i]
+#         return ""
+class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-        for i in range(len(words)):
-            if self.isPalindrome(words[i]) is True:
-                return words[i]
-        return ""
+        for i in words:
+            if i[::-1] == i:
+                return i
+        return ""   
