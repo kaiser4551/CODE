ad51e82e9bd45e0df5248c1a9b68f045f14cca4a
Tushar Jhod <115688635+kaiser4551@users.noreply.github.com>
2024-08-08 11:49:09 +0530

    Time: 42 ms (21.70%) | Memory: 16.6 MB (57.78%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,29 @@
+# class Solution:
+#     def subsets(self, nums: List[int]) -> List[List[int]]:
+#         a=[]
+#         n=len(nums)
+#         def sub(i,n,nums,arr):
+#             if i==0:
+#                 a.append(arr.copy())
+#                 return 
+#             sub(i-1,n,nums,arr)
+#             arr.append(nums[i-1])
+            
+#             sub(i-1,n,nums,arr)
+#             arr.pop()
+            
+#         arr=[]
+#         sub(n,n,nums,arr)
+#         return a
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        a=[]
         n=len(nums)
-        def sub(i,n,nums,arr):
-            if i==0:
-                a.append(arr.copy())
-                return 
-            sub(i-1,n,nums,arr)
-            arr.append(nums[i-1])
-            
-            sub(i-1,n,nums,arr)
-            arr.pop()
-            
         arr=[]
-        sub(n,n,nums,arr)
-        return a
+        for num in range(1<<n):
+            sub=[]
+            for i in range(n):
+                if (num &(1<<i)):
+                    sub.append(nums[i])
+            arr.append(sub)
+        return arr
+                
