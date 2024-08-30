da58b6ae5f0334612a466ff4c9575953cbb95f90
Tushar Jhod <115688635+kaiser4551@users.noreply.github.com>
2024-08-30 23:27:32 +0530

    Time: 167 ms (72.39%) | Memory: 18 MB (21.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,28 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         dic={}
+#         for num in nums:
+#             if num in dic:
+#                 dic[num]+=1
+#             else:
+#                 dic[num]=1
+#         n=len(nums)
+#         for num in dic:
+#             if dic[num]>n/2:
+#                 return num
+            
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic={}
+        count = 0
+        candidate = 0
+        
         for num in nums:
-            if num in dic:
-                dic[num]+=1
-            else:
-                dic[num]=1
-        n=len(nums)
-        for num in dic:
-            if dic[num]>n/2:
-                return num
+            if count == 0:
+                candidate = num
             
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        
+        return candidate
